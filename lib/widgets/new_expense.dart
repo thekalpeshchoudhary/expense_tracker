719166fd4317974ebd21fb6181b9629e64f85d75
Kalpesh Choudhary <kalpesh@retire100.live>
2023-06-16 23:23:30 +0530

    title input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -9,15 +9,18 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  void _titleInput(String value) {}
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20),
       child: Column(
-        children: const [
+        children: [
           TextField(
+            onChanged: _titleInput,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text('Expense Title'),
             ),
           ),
